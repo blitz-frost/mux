c32ad3a2816d5d9b30b9cf9421409b47171fef2a
Bogdan <mereu.etern@gmail.com>
2024-10-18 16:05:51 +0200

    - changed Path type to *struct{} - removed MakePath()

--- mux.go
+++ mux.go
@@ -14,50 +14,15 @@ type RW = sync.RWMutex
 // Functions that don't want to deal with deadlock analysis should take a Path as a parameter and use a Guard on it to prevent parallel executon.
 // This comes at a performance penalty compared to using a standard Mutex.
 //
-// A top-level caller should obtain a unique Path through MakePath(), and then release it through the Release() method.
-//
-// The zero Path is invalid. Guards will always lock it.
+// Any Path is valid, excluding nil.
 //
 // A specific Path must never be used asynchronously in multiple places. For example: for a Guard g and Path p, it must never happen that the method calls g.Guard(p) and g.Unguard(p) be in a race condition.
-type Path int
+type Path *struct{}
 
-// MakePath returns a usable unique Path.
-func MakePath() Path {
-	mux.Lock()
-	defer mux.Unlock()
-
-	for {
-		seek++
-		if seek == 0 {
-			seek++
-		}
-		if _, ok := paths[seek]; !ok {
-			break
-		}
-	}
-
-	paths[seek] = struct{}{}
-	return seek
-}
-
-// Release frees the Path for reuse.
-func (x Path) Release() {
-	mux.Lock()
-	defer mux.Unlock()
-
-	delete(paths, x)
-}
-
-type Guarder interface {
+type Guardian interface {
 	Guard(Path)
 	Unguard(Path)
 }
-
-var (
-	paths = make(map[Path]struct{})
-	seek  Path // last allocated Path
-	mux   S
-)
 
 // A Guard allows only one Path to proceed execution at a time.
 // Must not be copied after first use.
@@ -92,7 +57,7 @@ func (x *Guard) Lock() {
 // Calling Unguard on a Path that is not currently holding the lock will result in undefined behaviour (corrupt Guard).
 func (x *Guard) Unguard(p Path) {
 	if x.locks == 1 {
-		x.path = 0
+		x.path = nil
 		x.locks = 0
 		x.mux.Unlock()
 	} else {
@@ -173,7 +138,7 @@ func (x *RWGuard) RUnlock() {
 func (x *RWGuard) Unguard(p Path) {
 	x.wLocks--
 	if x.wLocks == 0 {
-		x.wPath = 0
+		x.wPath = nil
 		x.mux.Unlock()
 	}
 }
